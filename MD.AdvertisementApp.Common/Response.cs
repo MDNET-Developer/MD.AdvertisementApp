418db25cfb2e31eae5c04a014d6797648a49fdb5
MDNET-Developer <qarabagli.murad@mail.ru>
2022-11-22 16:23:41 +0400

    NewAdvertisementApp_ASP.NETCORE

---
+++ MD.AdvertisementApp.Common/Response.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD.AdvertisementApp.Common
+{
+    public class Response: IResponse
+    {
+        public Response(ResponseType responseType)
+        {
+            responseType = ResponseType;
+        }
+
+        public Response(ResponseType responseType, string message)
+        {
+            responseType = ResponseType;
+            message = Message;
+        }
+
+        public string Message { get; set; }
+        public ResponseType ResponseType { get; set; }
+    }
+
+    public enum ResponseType
+    {
+        Succsess,
+        ValidationError,
+        NotFound
+    }
+}
---
+++ MD.AdvertisementApp.Common/Response{T}.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD.AdvertisementApp.Common
+{
+    public class Response<T> : Response
+    {
+        public T Data { get; set; }
+        public List<CustomValidationError> Errors { get; set; }
+        public Response(ResponseType responseType, string message) : base(responseType, message)
+        {
+        }
+
+        public Response(ResponseType responseType, T data) : base(responseType)
+        {
+            Data = data;
+        }
+
+        public Response(ResponseType responseType, T data, List<CustomValidationError> errors) : base(responseType)
+        {
+            Errors = errors;
+            Data = data;
+        }
+    }
+}
