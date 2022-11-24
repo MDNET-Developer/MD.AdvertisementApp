b1fc9dcb2280a3201433d4e4fc2824ccd1fa3ebb
MDNET-Developer <qarabagli.murad@mail.ru>
2022-11-24 10:11:20 +0400

    _completeEntityLayer

---
+++ MD.AdvertisementApp.Entities/AppRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD.AdvertisementApp.Entities
+{
+    public class AppRole : BaseEntity
+    {
+        public string Definiton { get; set; }
+        public List<AppUserRole> AppUserRoles { get; set; }
+    }
+}
---
+++ MD.AdvertisementApp.Entities/AppUserRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD.AdvertisementApp.Entities
+{
+    public class AppUserRole
+    {
+        public int AppUserId { get; set; }
+        public AppUser appUser{ get; set; }
+
+        //---------------------------------------------------------------------
+        public int AppRoleId { get; set; }
+        public AppRole  appUserRole { get; set; }
+    }
+}
