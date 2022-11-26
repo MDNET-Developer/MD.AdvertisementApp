62a636d101ad828a56d9c3bfa7217c8f28ffc4da
MDNET-Developer <qarabagli.murad@mail.ru>
2022-11-26 20:14:41 +0400

    SomeUpdateDataAccsessGenericRepository

---
+++ MD.AdvertisementApp.DataAccess/Interface/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using MD.AdvertisementApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD.AdvertisementApp.DataAccess.Interface
+{
+    public interface IGenericRepository<T> where T: BaseEntity
+    {
+    }
+}
